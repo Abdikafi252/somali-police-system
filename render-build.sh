c318974dc3911d3845c25c5333ccad496a22a625
Abdikafi252 <abdikafi252@github.com>
2026-02-03 04:12:12 +0300

    Add Render.app deployment script

---
+++ render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+composer install --no-dev --optimize-autoloader
+
+# Run database migrations
+# php artisan migrate --force
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
