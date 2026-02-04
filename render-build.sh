c1166bde2ec108d7ff49c98158be65cf5d14c11c
Abdikafi252 <abdikafi252@github.com>
2026-02-04 20:13:47 +0300

    Fix: Force clean storage link & explicit chmod in yaml

--- render-build.sh
+++ render-build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install PHP dependencies
 composer install --no-dev --optimize-autoloader --no-interaction
 
+# Force remove public/storage to prevent "link exists" errors
+rm -rf public/storage
+
 # Create storage link
 php artisan storage:link
 
