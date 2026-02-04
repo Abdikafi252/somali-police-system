69a75a54eb15687ea3a8587ab9524d718aa6ce95
Abdikafi252 <abdikafi252@github.com>
2026-02-04 19:10:09 +0300

    Fix: Absolute minimal build script (Composer ONLY)

--- render-build.sh
+++ render-build.sh
@@ -4,12 +4,3 @@ set -o errexit
 
 # Run composer install with NUCLEAR safety (Ignore PHP version checks)
 composer install --no-dev --optimize-autoloader --no-interaction --no-scripts --ignore-platform-reqs
-
-# Run database migrations
-# Skip cache commands during build (Environment vars may not be ready)
-# php artisan config:cache
-# php artisan route:cache
-# php artisan view:cache
-
-# Create symbolic link for storage
-php artisan storage:link
