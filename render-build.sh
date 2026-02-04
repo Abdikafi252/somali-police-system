a42c9a9e306dde82cf35c9035b07cffe00d501ba
Abdikafi252 <abdikafi252@github.com>
2026-02-04 19:24:47 +0300

    Fix: Force LF line endings & minimal build script

--- render-build.sh
+++ render-build.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
+# Minimal build script
 
-# Run composer install with ULTRA safety (No scripts, No interaction, No version check)
-composer install --no-dev --optimize-autoloader --no-interaction --no-scripts --ignore-platform-reqs
-
-# Run database migrationsn
-# php artisan migrate --force
-
-# php artisan config:cache
-# php artisan route:cache
-# php artisan view:cache
-
-# Create symbolic link for storage
-php artisan storage:link
+composer install --no-dev --optimize-autoloader
