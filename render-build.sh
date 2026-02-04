1ed88a734697d922abcef9a04fe6914e4a4b240b
Abdikafi252 <abdikafi252@github.com>
2026-02-04 19:13:57 +0300

    Fix: Revert build script to 5:30 PM version

--- render-build.sh
+++ render-build.sh
@@ -2,11 +2,12 @@
 # exit on error
 set -o errexit
 
-# Run composer install with NUCLEAR safety (Ignore PHP version checks)
-composer install --no-dev --optimize-autoloader --no-interaction --no-scripts --ignore-platform-reqs
+# Run composer install
+composer install --no-dev --optimize-autoloader
 
-# Run database migrations (Try to run, but don't fail build if DB is offline)
-php artisan migrate --force || true
+# Run database migrations
+# php artisan migrate --force
+
 # php artisan config:cache
 # php artisan route:cache
 # php artisan view:cache
