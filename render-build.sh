01012c6f9e9d881624861db24a7d5abac71631e7
Abdikafi252 <abdikafi252@github.com>
2026-02-04 19:06:41 +0300

    Fix: Update build script to user's preferred version

--- render-build.sh
+++ render-build.sh
@@ -2,5 +2,15 @@
 # exit on error
 set -o errexit
 
-# Run composer install with NUCLEAR safety (Ignore PHP version checks)
-composer install --no-dev --optimize-autoloader --no-interaction --no-scripts --ignore-platform-reqs
+# Run composer install
+composer install --no-dev --optimize-autoloader
+
+# Run database migrations
+# php artisan migrate --force
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Create symbolic link for storage
+php artisan storage:link
