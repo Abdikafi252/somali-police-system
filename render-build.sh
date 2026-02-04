99ba9ae7e5145cfe88cd051258ec1be5e32ffcd8
Abdikafi252 <abdikafi252@github.com>
2026-02-04 19:08:01 +0300

    Fix: Revert to Nuclear Composer Command (ignore-platform-reqs)

--- render-build.sh
+++ render-build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-# Run composer install
-composer install --no-dev --optimize-autoloader
+# Run composer install with NUCLEAR safety (Ignore PHP version checks)
+composer install --no-dev --optimize-autoloader --no-interaction --no-scripts --ignore-platform-reqs
 
 # Run database migrations
 # php artisan migrate --force
